c0a3759df0b1c847b0b716eb42dd928e6e355536
nexe <87233162+Nexe08@users.noreply.github.com>
2021-10-29 09:07:41 +0530

    Delete Object directory

--- Object/Effect/Explostion_1.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-
-
-func _on_AnimationPlayer_animation_finished(_anim_name: String) -> void:
-    queue_free()
--- Object/GroundEntitySpawnPositions/GroundEntitySpawnPositions.gd
+++
@@ -1,5 +0,0 @@
-extends Node2D
-# Ground Entity Spawn Positions
-
-func _ready() -> void:
-    Global.GroundEntitySpawnPositions = self
